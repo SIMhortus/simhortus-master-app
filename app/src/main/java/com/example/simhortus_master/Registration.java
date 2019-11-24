920d19db7e5fcdc152a28599da2904b648979178
iamDete <31462246+iamDete@users.noreply.github.com>
2019-11-24 18:56:58 +0800

    UI Form done!

--- app/src/main/java/com/example/simhortus_master/Registration.java
+++ app/src/main/java/com/example/simhortus_master/Registration.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
 
     }
-    public void goToReg(View v){
-        startActivity(new Intent(this, Registration.class));
+
+    public void goToLogIn(View v){
+        startActivity(new Intent(this, MainActivity.class));
     }
 
 }
