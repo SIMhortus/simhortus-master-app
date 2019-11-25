b1030a3c78b26e5fed3e09c42d942653c183d56c
Roselle <roselleroxtabuena@gmail.com>
2019-11-25 10:57:50 +0800

    mod email custom dialog

--- app/src/main/java/com/example/simhortus_master/AccountSettings.java
+++ app/src/main/java/com/example/simhortus_master/AccountSettings.java
@@ -3,9 +3,11 @@ package com.example.simhortus_master;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,11 +22,11 @@ import com.google.firebase.auth.FirebaseUser;
 
 
 
-public class AccountSettings extends AppCompatActivity {
+public class AccountSettings extends AppCompatActivity implements View.OnClickListener {
 
     TextView textView;
     private FirebaseAuth mAuth;
-    Button button;
+    private Button btnLogout, btnEmail, btnPass;
 
     private AwesomeValidation awesomeValidation;
 
@@ -34,19 +36,15 @@ public class AccountSettings extends AppCompatActivity {
         setContentView(R.layout.activity_account_settings);
 
         textView = findViewById(R.id.textViewVerified);
-        button = findViewById(R.id.logout);
+
+        btnLogout = findViewById(R.id.logout);
+        btnEmail = findViewById(R.id.updEmail);
+        btnPass = findViewById(R.id.updPass);
+
         mAuth = FirebaseAuth.getInstance();
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth.getInstance().signOut();
-                startActivity(new Intent(AccountSettings.this, MainActivity.class));
-            }
-        });
-
+        initClick();
 
     }
 
@@ -71,13 +69,58 @@ public class AccountSettings extends AppCompatActivity {
                     user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            Toast.makeText(AccountSettings.this, "Verification Email sent", Toast.LENGTH_LONG).show();
+                            Toast.makeText(AccountSettings.this,
+                                    "Verification Email sent",
+                                    Toast.LENGTH_LONG).show();
                         }
                     });
                 }
             });
         }
 
+    }
+
+    public void initClick() {
+        btnLogout.setOnClickListener(this);
+        btnEmail.setOnClickListener(this);
+    }
+
+    public void ShowPopupEmail() {
+        Dialog myDialog = new Dialog(this);
+        myDialog.setContentView(R.layout.mod_email);
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button update = findViewById(R.id.btnEMUpdate);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(AccountSettings.this,
+                        " Email sent",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        myDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.logout:
+                mAuth.getInstance().signOut();
+                startActivity(new Intent(AccountSettings.this, MainActivity.class));
+                break;
+
+            case R.id.updEmail:
+                    ShowPopupEmail();
+                break;
+
+        }
     }
 
 }
