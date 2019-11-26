43a29b7dd360c202dc97ecba57e7c84cfb644c13
iamDete <31462246+iamDete@users.noreply.github.com>
2019-11-26 16:24:36 +0800

    On going scan code activity

--- app/src/main/java/com/example/simhortus_master/ScanCodeActivity.java
+++ app/src/main/java/com/example/simhortus_master/ScanCodeActivity.java
@@ -6,18 +6,25 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.Result;
-
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
     ZXingScannerView ScannerView;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         ScannerView = new ZXingScannerView(this);
         setContentView(ScannerView);
+
+        mAuth = FirebaseAuth.getInstance();
 
         this.requestPermissions(new String[]{Manifest.permission.CAMERA}, 1011);
     }
@@ -34,8 +41,20 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
     public void handleResult(Result result) {
         final String dID = result.getText();
 
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.hasChild("name")) {
+                    // run some code
+                }
+            }
+        });
+
         //ScanGarden.resultTextView.setText(result.getText());
         // put your code here
+
 
 
     }
